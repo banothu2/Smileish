0a35a28d525abaf7ff51c44c1acb756e75cc0278
banothu2 <banothu2@illinois.edu>
2015-01-18 03:16:15 -0600

    Merging with iOS Smile detection

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
+var sys = require('sys')
+var exec = require('child_process').exec;
+
 var databaseUrl = "smileish"; // "username:password@example.com/mydb"
-var collections = ["responses"]
+var collections = ["responses", "bananas"]
 var db = require("mongojs").connect(databaseUrl, collections);
 
 
@@ -18,6 +21,57 @@ router.post('/upload/path', function(req, res){
     console.log(req.body);
 
 });
+
+
+router.get('/kickoffSmiles', function(req, res){
+	function puts(error, stdout, stderr) { sys.puts(stdout) }
+	var date;
+	date = exec("date", puts);
+	res.send("OK");
+})
+
+router.get("/api/smileish/:response", function(req, res){
+	var resp = req.params.response;
+
+	db.bananas.save(
+		{
+			values: resp
+		}, function(err, saved) {
+		  if( err || !saved ) console.log("response not saved");
+		  else console.log("response saved");
+		}
+	);
+})
+
+router.get("/api/allVideos/:videoID", function(req, res){
+	var videoID = req.params.videoID;
+	db.responses.find({videoID: videoID}, function(err, response){
+		var ids = [];
+		if( err || !response){
+			res.send({});
+		} 
+		else {
+			response.forEach( function(resp) {
+				ids.push(resp.videoToken);
+		  	} );
+		  	res.send(ids)
+		}
+	})
+})
+
+router.get("/api/getLatest", function(req, res){
+	var value = "banana";
+
+	db.bananas.find({}).sort({x:-1}, function(err, docs) {
+		var latest = [];
+		var count = 1;
+	    docs.forEach(function(resp){
+	    	latest.push(resp.values);
+	    })
+	    res.send(latest[0]);
+	});
+
+})
 
 
 // router.get('/api/base64/:base64', function(req, res){
